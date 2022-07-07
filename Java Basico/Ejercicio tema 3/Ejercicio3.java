74f4c6a0ad38f8ab0f7e0d75e9a8990178a79457
Keny J <89676438+Keny4747@users.noreply.github.com>
2022-07-07 10:32:11 -0500

    Create Ejercicio3.java

---
+++ Java Basico/Ejercicio tema 3/Ejercicio3.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Marcos", "Felipe", "Carlos", "Antonio", "Daniel"};
+
+        for (String lista : nombres) {
+            System.out.print(lista + " ");
+        }
+
+    }
+
+
+}
