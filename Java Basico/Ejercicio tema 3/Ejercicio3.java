81cdd0d2c9c1e8855fd4248a068bbfe7b44138ae
Keny J <89676438+Keny4747@users.noreply.github.com>
2022-07-07 10:32:45 -0500

    Update Ejercicio3.java

--- Java Basico/Ejercicio tema 3/Ejercicio3.java
+++ Java Basico/Ejercicio tema 3/Ejercicio3.java
@@ -4,7 +4,8 @@ public class Ejercicio3 {
 
     public static void main(String[] args) {
         String[] nombres = {"Marcos", "Felipe", "Carlos", "Antonio", "Daniel"};
-
+        
+        //imprimir en consola a través del bucle for each
         for (String lista : nombres) {
             System.out.print(lista + " ");
         }
